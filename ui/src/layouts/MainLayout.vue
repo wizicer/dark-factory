80f443a5367bf8014515a94fdd608d2d25b0677f
Icer Liang <liangshuang@sutu.tech>
2024-11-16 16:05:28 +0700

    add wallet connect button

--- ui/src/layouts/MainLayout.vue
+++ ui/src/layouts/MainLayout.vue
@@ -17,6 +17,37 @@
           <q-btn flat dense round icon="email" />
           <q-btn flat dense round icon="settings" />
         </div>
+        <div>
+          <ConnectWalletButton
+            :address="address"
+            :txnCount="0"
+            :dark="false"
+            @click="connect"
+          >
+            <!-- Override the default text on the "Connect Wallet" button -->
+            Connect Wallet!
+
+            <template #pending>
+              <!-- Override the blue pending transaction div -->
+            </template>
+
+            <template #spinner>
+              <!-- Use your own Spinner (svg, css, image, etc.) -->
+            </template>
+
+            <template #identicon>
+              <!-- Use your own identicon library. Jazzicon is the default -->
+            </template>
+
+            <template #connectWalletButton>
+              <!-- Use your own button for the "Connect Wallet" state when no address is provided -->
+            </template>
+
+            <template #addressButton>
+              <!-- Use your own button to display the address when address is provided. Does not remove the pending transaction div -->
+            </template>
+          </ConnectWalletButton>
+        </div>
       </q-toolbar>
     </q-header>
 
@@ -63,6 +94,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { EssentialLinkProps } from 'components/EssentialLink.vue';
+import { ConnectWalletButton, useMetaMaskWallet } from 'vue-connect-wallet';
 
 import { useMeta } from 'quasar';
 
@@ -100,4 +132,40 @@ const leftDrawerOpen = ref(false);
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
+
+const wallet = useMetaMaskWallet();
+// console.log(wallet);
+
+const address = ref('');
+
+wallet.onAccountsChanged((accounts: string[]) => {
+  console.log('account changed to: ', accounts[0]);
+});
+wallet.onChainChanged((chainId: number) => {
+  console.log('chain changed to:', chainId);
+});
+
+const connect = async () => {
+  console.log('connect');
+  const accounts = await wallet.connect();
+  if (typeof accounts === 'string') {
+    console.log('An error occurred' + accounts);
+  }
+  address.value = accounts[0];
+};
+
+// const switchAccount = async () => {
+//   await wallet.switchAccounts();
+//   connect();
+// };
+
+const isConnected = async () => {
+  const accounts = await wallet.getAccounts();
+  if (typeof accounts === 'string') return false;
+  return accounts.length > 0;
+};
+
+isConnected().then((value) => {
+  if (value) connect();
+});
 </script>
---
+++ ui/src/vue-connect-wallet.d.ts
@@ -0,0 +1,6 @@
+declare module 'vue-connect-wallet' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export const ConnectWalletButton: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export const useMetaMaskWallet: any;
+}
